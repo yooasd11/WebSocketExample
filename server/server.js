2346db845e1e4a4d096a4c740b9c98bb6a35b6da
yooasd1 <yooasd11@hotmail.com>
2018-01-14 22:12:42 +0900

    add server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname+'/index.html');
+})
+
+io.on('connection', function(socket) {
+	console.log('A user connected : ' + socket.id);
+	socket.on('message', function(data) {
+		console.log(data);
+		io.emit('message', { message: data });
+	})
+	socket.on('disconnect', function() {
+		console.log("A user disconnected : " + socket.id);
+	})
+})
+
+http.listen(3000, function() {
+	console.log('Server listening on port 3000');
+})
